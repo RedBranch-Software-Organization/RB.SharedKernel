405a443626a0561a489454827331b345eb667762
Rawenvoys <lukwis95@gmail.com>
2025-02-12 04:34:20 +0100

    Implement IsBetween

--- src/RB.SharedKernel/Extensions/DateTimeExtensions.cs
+++ src/RB.SharedKernel/Extensions/DateTimeExtensions.cs
@@ -2,5 +2,6 @@ namespace RB.SharedKernel.Extensions;
 public static class DateTimeExtensions
 {
     public static bool IsBetween(this DateTime value, DateTime startDate, DateTime endDate) 
-        => startDate >= value && value <= endDate;
+        => (startDate <= value && value <= endDate) 
+        || (endDate <= value && value <= startDate);
 }
